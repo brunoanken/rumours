4c2922453e7e016f6a3b5c9a770de536897a0546
Bruno <brunoanken@gmail.com>
2022-06-03 08:04:57 -0300

    Create User schema

---
+++ lib/rumours/accounts/user.ex
@@ -0,0 +1,36 @@
+defmodule Rumours.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :confirmed_at, :naive_datetime
+    field :password_hash, :string
+    field :username, :string
+
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  @email_regex ~r/(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :username, :password])
+    |> validate_required([:email, :username, :password])
+    |> validate_format(:email, @email_regex)
+    |> validate_length(:password, min: 5)
+    |> validate_length(:username, max: 20)
+    |> put_password_hash()
+  end
+
+  def put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  def put_password_hash(changeset), do: changeset
+end
---
+++ test/rumours/accounts/user_test.exs
@@ -0,0 +1,65 @@
+defmodule Rumours.Accounts.UserTest do
+  use Rumours.DataCase, async: true
+  alias Rumours.Accounts.User
+
+  @valid_attrs %{
+    email: "email@mail.com",
+    password: "securep@ss",
+    username: "Biruleibe"
+  }
+
+  @invalid_attrs %{
+    email: "tafi.com",
+    password: ""
+  }
+
+  describe "changeset/2" do
+    test "returns with errors when email is not provided" do
+      changeset = User.changeset(%User{}, %{email: ""})
+      assert %{email: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns with errors when username is not provided" do
+      changeset = User.changeset(%User{}, %{username: ""})
+      assert %{username: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns with errors when password is not provided" do
+      changeset = User.changeset(%User{}, %{password: ""})
+      assert %{password: ["can't be blank"]} = errors_on(changeset)
+    end
+
+    test "returns with errors when password is not at least five characters long" do
+      changeset = User.changeset(%User{}, %{password: "1234"})
+      assert %{password: ["should be at least 5 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns with errors when email does not match the required format" do
+      changeset = User.changeset(%User{}, %{email: "almost@validcom"})
+      assert %{email: ["has invalid format"]} = errors_on(changeset)
+    end
+
+    test "returns with errors when username is longer than 20 characters" do
+      changeset = User.changeset(%User{}, %{username: "loooooooooooooooooong"})
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+    end
+
+    test "returns with a password hash when data is valid" do
+      %Ecto.Changeset{changes: %{password_hash: password_hash}} =
+        User.changeset(%User{}, @valid_attrs)
+
+      assert password_hash != ""
+      refute is_nil(password_hash)
+    end
+  end
+
+  describe "put_password_hash/1" do
+    test "puts the password_hash when changeset is valid" do
+      %Ecto.Changeset{changes: %{password_hash: password_hash}} =
+        %User{} |> User.changeset(@valid_attrs) |> User.put_password_hash()
+
+      assert password_hash != ""
+      refute is_nil(password_hash)
+    end
+  end
+end
